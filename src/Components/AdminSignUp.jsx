fd9bfeb9412feddc5edd3ea96db7521efd43aa96
boobalansanjai4@gmail.com <boobalan2608>
2025-01-20 15:17:44 +0530

    Updating..

--- src/Components/AdminSignUp.jsx
+++ src/Components/AdminSignUp.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 import '../Styles/AdminSignup.css';
 import axios from 'axios';
@@ -8,30 +8,44 @@ const AdminSignUp = () => {
     let [adminName, setAdminname] = useState("");
     let [password, setPassword] = useState("");
     let [name, setName] = useState("");
-    console.log(adminName);
-    console.log(password);
-    let data = {adminName,password,name};
-    function AddAdminData(){
-            axios.post("https://ecommercedemodata-1.onrender.com/AdminInfo",data)
-            .then((res)=>{
-                toast.success("Admin Added Successfully")
-            })
-            .catch((err)=>{
-                alert(err);
-            })
+    let [adminData, setAdminData] = useState({});
+    let data = { adminName, password, name };
+    useEffect(() => {
+        if (adminName) {
+            axios.get(`https://ecommercedemodata-1.onrender.com/AdminInfo?adminName=${adminName}`)
+                .then((res) => {
+                    setAdminData(res.data);
+                })
+                .catch(() => {
+                    console.log("Data not present");
+                })
+        }
+    }, [adminName])
+    function AddAdminData() {
+        if (!adminData) {
+            axios.post("https://ecommercedemodata-1.onrender.com/AdminInfo", data)
+                .then((res) => {
+                    toast.success("Admin Added Successfully")
+                })
+                .catch((err) => {
+                    alert(err);
+                })
+        } else {
+            toast.error("Admin Name is already Exisit..")
+        }
     }
     return (
         <div className="admin-signup">
             <h1 className="admin-title">Admin SignUp Page</h1>
             <div className="admin-signup-form-cont">
-            <div className="img-cont"> <img src={require("../Images/adminSignUp.png")} alt="admin signup" /></div>
-                <form action=""  onSubmit={AddAdminData} className="admin-signup-form">
+                <div className="img-cont"> <img src={require("../Images/adminSignUp.png")} alt="admin signup" /></div>
+                <form action="" onSubmit={AddAdminData} className="admin-signup-form">
                     <label>Admin Name: </label>
-                    <input type="text" onChange={(e) => { setAdminname(e.target.value) }} className="admin-username" placeholder="Enter the UserName" required/>
+                    <input type="text" onChange={(e) => { setAdminname(e.target.value) }} className="admin-username" placeholder="Enter the UserName" required />
                     <label htmlFor="">Name: </label>
-                    <input type="text" onChange={(e) => { setName(e.target.value) }} className="amdin-name" placeholder="Enter the Name" required/>
+                    <input type="text" onChange={(e) => { setName(e.target.value) }} className="amdin-name" placeholder="Enter the Name" required />
                     <label htmlFor="">Password: </label>
-                    <input type="text" onChange={(e) => { setPassword(e.target.value) }} className="amdin-pass" placeholder="Enter the Password" required/>
+                    <input type="text" onChange={(e) => { setPassword(e.target.value) }} className="amdin-pass" placeholder="Enter the Password" required />
                     <button className="submit-btn">Sign up</button>
                 </form>
             </div>
